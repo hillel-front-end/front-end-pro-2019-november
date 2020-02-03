69f972e561e452d5c9099859be64ec12418e314b
VTernavskiy <Valeriy.Ternavskiy@alfabank.kiev.ua>
2020-02-03 18:04:58 +0200

    l-23-upd

---
+++ lections/lection_23_VUE_Intro/materials/main.js
@@ -0,0 +1,27 @@
+console.log('Lection 23 __ VUE INTRO __');
+
+/*
+1. Instance
+2. {{}} Interpolations (https://ru.vuejs.org/v2/guide/syntax.html)
+3. Expression in nterpolations({{?:}})
+3. directives: 
+    * v-html="'String with html value'"
+    * v-bind:disabled="boolean"
+    * v-bind:href="url"
+    * v-model
+    * v-if="boolean" - v-else-if="boolean" - v-else
+    * v-show
+    * v-for - loop (Work like  for-in), (value, name, index)
+
+4. styles directives:
+   * v-bind:class="{ 'class-name as string': Boolean, 'another-class-name': Boolean variable, .., }"
+   * v-bind:class="['activeClass', errorClassAsVar]"
+   * v-bind:style="{ cssPropName: activeColorAsVar, fontSize: n + 'px', matgin: '10px' }"
+5. v-on:click,mousenove .. (@click)
+
+6. $set(vm.items, indexOfItem, newValue)
+*/
+
+window.onload = () => {
+    const config = {};
+}
