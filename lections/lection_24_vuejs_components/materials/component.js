3dfef9f08448ab19600ebfa5e9d200a23fc42e54
Ihor Hilei <igorgiley@gmail.com>
2020-02-10 20:15:36 +0200

    props

---
+++ lections/lection_24_vuejs_components/materials/component.js
@@ -0,0 +1,66 @@
+
+Vue.component('app-button', {
+    props:  {
+        foo: Object,
+        className: String
+    },
+    data() {
+        return {
+            name: 'Gallery'
+        }
+    },
+    template: `
+        <span :class="['navigation__item', className]">
+            {{foo? foo.name : 'Foo'}}
+        </span>
+    `
+})
+
+Vue.component('navigation', {
+    data() {
+        return {
+            buttons: [
+                {name: 'Main'},
+                {name: 'Contacts'},
+                {name: 'Email'}
+            ]
+        }
+    },
+    template: `
+        <div class="navigation">
+            <app-button 
+                v-for="buttonItem in buttons" 
+                :foo="buttonItem" />
+        </div>
+    `
+});
+
+
+
+Vue.component('main-app', {
+    data() {
+        return {
+            appName: 'Hello world'
+        }
+    },
+    template: `
+    <div>
+        <header>
+            <navigation />
+            <app-button 
+            :className="'button_className'"
+            :foo="{name: 'Candidates'}"/>
+        </header>
+    </div>
+      `
+}); // comp-name, config
+
+new Vue({
+    el:'#app',
+    data: {
+      name: "Valera",
+      list: ["red"]
+    }
+});
+
+
