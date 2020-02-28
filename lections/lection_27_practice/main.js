28656730066a22807556f824c3244c9413af9c3f
Ihor Hilei <igorgiley@gmail.com>
2020-02-28 20:01:43 +0200

    l-27

--- lections/lection_27_practice/main.js
+++ lections/lection_27_practice/main.js
@@ -1,13 +1,19 @@
-function sendAjax(url, method) {
+function sendAjax(method, url) {
     return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        if(true) {
-            resolve();
-        } else {
-            reject();
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+        xhr.send();
+
+
+        xhr.onreadystatechange = function() { // (3)
+            console.log(xhr.responseText, xhr.readyState,'xhr.responseText');
+            if(xhr.readyState == 4) {
+                let response = JSON.parse(xhr.responseText);
+                resolve(response);
+            }   
         }
 
-    })
+    });
 }
 
 
